78a1e79f915753a9bfd56dcce231001d3e76fe19
Joe Gomez <jgomez@zehnergroup.com>
2020-04-09 14:42:32 -0300

    MUN-17 | Feature | Build hero banner module

---
+++ src/_scripts/sections/heroSection.js
@@ -0,0 +1,39 @@
+import $ from 'jquery'; // eslint-disable-line no-unused-vars
+import BaseSection from './base';
+import VideoPlayer from '../ui/videoPlayer';
+
+const selectors = {
+  videoPlayer: '[data-video-player]'
+};
+
+export default class FooterSection extends BaseSection {
+  constructor(container) {
+    super(container, 'hero');
+
+    this.$container = $(container);
+
+    this.background = this.$container.data('background-color');
+
+    if ($(selectors.videoPlayer).length) {
+      this.player = new VideoPlayer($(selectors.videoPlayer, this.$container));
+    }
+
+    this.observerProperties = {
+      root: null,
+      threshold: 0.4
+    }
+
+    this.IntersectionObserver = new IntersectionObserver(this.observerCallback.bind(this), this.observerProperties);
+
+    this.IntersectionObserver.observe(this.$container.get(0));
+
+  }
+
+  observerCallback(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0.4 && this.background !== '') {
+        $('body').css('background-color', this.background);
+      }
+    })
+  }
+}
