b2aaccbc32dffd4e894b5ececd68303eec59a9eb
Cory Sturgis-Casturgi <casturgi@gmail.com>
2020-12-15 19:46:14 -0800

    feature
    marquee pencil banner

--- src/_scripts/sections/pencilBanner.js
+++ src/_scripts/sections/pencilBanner.js
@@ -1,8 +1,24 @@
 import $ from 'jquery'; // eslint-disable-line no-unused-vars
 import BaseSection from './base';
 
+const selectors = {
+  marqueeContentContainer: '[data-marquee-content-container]'
+};
+
 export default class PencilBannerSection extends BaseSection {
   constructor(container) {
     super(container, 'pencilBanner');
+
+    this.$container = $(container);
+
+    this.$container.on('touchstart touchend mouseenter mouseleave', this.onTouch.bind(this))
+  }
+
+  onTouch(e) {  
+    if (e.type == 'touchstart' || e.type == 'mouseenter') {
+      $(selectors.marqueeContentContainer, this.$container).addClass('marquee-paused')
+    } else {
+      $(selectors.marqueeContentContainer, this.$container).removeClass('marquee-paused')
+    }
   }
 }
