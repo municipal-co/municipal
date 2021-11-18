7f58a079c063dad40bd304c6165e306566c85792
betown <joe.gomezr@gmail.com>
2021-11-18 19:03:54 -0300

    Added logict to update cart based on the cart value or feature availability

--- src/_scripts/core/cartAPI.js
+++ src/_scripts/core/cartAPI.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import * as Currency from './currency';
 import * as Image from './image';
+import AJAXFormManager from '../managers/ajaxForm';
 
 class CartAPI {
   constructor() {
@@ -102,7 +103,11 @@ class CartAPI {
       success: (data) => {
         // Theme editor adds HTML comments to JSON response, strip these
         data = data.replace(/<\/?[^>]+>/gi, '');
-        const cart = JSON.parse(data);
+        let cart = JSON.parse(data);
+
+        const newCart = this.validateGiftThreshold(cart);
+
+        cart = newCart;
 
         this.cart = this.formatCart(cart);
 
@@ -153,14 +158,14 @@ class CartAPI {
    * @param {jQuery} $form - jQuery instance of the form
    * @return {Promise} - Resolve returns JSON cart | Reject returns an error message
    */
-  addItemFromID(id) {
+  addItemFromID(id, properties = {}) {
     const promise = $.Deferred();
 
     $.ajax({
       type: 'post',
       dataType: 'json',
       url: '/cart/add.js',
-      data: { quantity: 1, id: id },
+      data: { quantity: 1, id: id, properties: properties},
       success: () => {
         this.getCart().then((cart) => {
           promise.resolve(cart);
@@ -210,7 +215,7 @@ class CartAPI {
       },
       error: () => {
         const data = {
-          message: 'Something went wrong.'
+          message: 'Something went wrong. With the line'
         };
         promise.reject(data);
       }
@@ -229,6 +234,40 @@ class CartAPI {
    */
   changeLineItemQuantityByKey(key, qty) {
     return this.changeLineItemQuantity(this.getCartLineByItemKey(key), qty);
+  }
+
+  validateGiftThreshold(cart) {
+    let hasFreeGift = false;
+    let freeGiftIndex = -1;
+    let itemsArrayIndex = -1;
+    const freeGiftEnabled = theme.hasOwnProperty('giftWithPurchase');
+    let removeFreeGift = false;
+
+    cart.items.forEach((item, index) => {
+      if(item.properties.hasOwnProperty('_freeGift')) {
+        hasFreeGift = true;
+        itemsArrayIndex = index;
+        freeGiftIndex = index + 1;
+      }
+    })
+
+    if(freeGiftEnabled === true) {
+      const giftThreshold = theme.giftWithPurchase.giftThreshold;
+      if(cart.total_price < giftThreshold) {
+        removeFreeGift = true;
+      }
+    } else {
+      removeFreeGift = true;
+    }
+
+    if(removeFreeGift && hasFreeGift) {
+      this.changeLineItemQuantity(freeGiftIndex, 0).then((newCart)=>{
+        const event = $.Event(AJAXFormManager.events.ADD_SUCCESS, {cart: newCart});
+        $(window).trigger(event);
+      })
+    }
+
+    return cart;
   }
 }
 
