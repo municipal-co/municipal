6827a015e24748e8c29b5b53cc92e1a7c2b04e96
betown <joe.gomezr@gmail.com>
2022-07-18 10:17:35 -0300

    WIP - Add zoom to product gallery

--- src/_scripts/view/product/productDetailGalleries.js
+++ src/_scripts/view/product/productDetailGalleries.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import Swiper from 'swiper';
 import BaseSection from '../../sections/base';
 
@@ -8,17 +9,25 @@ const selectors = {
 
 const classes = {
   gallerySlide: 'swiper-slide',
+  zoomedIn: 'is-zoomed',
+  zoomReady: 'is-zoomable'
 };
 
 export default class productGallery extends BaseSection {
   constructor(container) {
     super(container, 'productGallery');
+    const self = this;
     this.$container = document.querySelector(selectors.productGallery)
     this.$slides = this.$container.querySelectorAll(selectors.gallerySlide);
     this.currentColor = '';
     this.gallerySettings = {
       slidesToShow: 1,
       autoplay: false,
+      zoom: {
+        maxRatio: 5,
+        minRatio: 1,
+        toggle: true,
+      },
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -32,14 +41,13 @@ export default class productGallery extends BaseSection {
       },
       loop: true,
       threshold: 40,
-      effect: 'fade'
+      effect: 'fade',
+      init: false,
     }
 
-    this.initGallery();
-  }
-
-  initGallery() {
     this.slider = new Swiper(this.$container, this.gallerySettings);
+    // this.slider.on('slideChange', this.onSlideChange.bind(this));
+    this.slider.init();
   }
 
   onVariantChange(variant) {
@@ -67,7 +75,36 @@ export default class productGallery extends BaseSection {
       // Update variant images here
       this.slider = new Swiper(this.$container, this.gallerySettings);
     }
+  }
 
+  onSlideChange() {
+    console.log(this.slider.slides[this.slider.activeIndex]);
+
+    this.initZoom(this.slider.slides[this.slider.activeIndex]);
+  }
+
+  initZoom(slide) {
+    $(slide).zoom({
+      url: $(slide).data('image-url'),
+      on: 'click',
+      target: '.product-gallery__zoom-container',
+      touch: false,
+      escToClose: true,
+      magnify: 1,
+      duration: 300,
+      callback: () => {
+        slide.classList.add(classes.zoomReady);
+
+      },
+      onZoomIn: () => {
+        slide.classList.add(classes.zoomedIn);
+        document.querySelector('[data-zoom-container]').classList.add(classes.zoomedIn);
+      },
+      onZoomOut: () => {
+        slide.classList.remove(classes.zoomedIn);
+        document.querySelector('[data-zoom-container]').classList.remove(classes.zoomedIn);
+      }
+    })
   }
 
   destroy() {
