b15d756565b227360725d2b1eca3f4cf118726b5
betown <joe.gomezr@gmail.com>
2022-09-28 11:44:14 -0300

    Inteagrate toggle behavior with navigation toggle button

--- src/_scripts/view/navigation/navigation.js
+++ src/_scripts/view/navigation/navigation.js
@@ -87,14 +87,28 @@ const MainNav = ((props) => {
   }
 
   const toggleNavigation = () => {
-    setIsOpen(!isOpen);
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  const updateHeaderOffset = () => {
+    setStyles({
+      top: header.offsetHeight,
+      height: `calc(100% - ${header.offsetHeight}px)`
+    })
   }
 
   const [isOpen, setIsOpen] = useState(false);
   const [data, setData] = useState(getComponentData());
-  const navBody = useRef(null);
+  const element = useRef();
+  const navBody = useRef();
+  const header = document.querySelector('[data-header]');
+  const [styles, setStyles] = useState({
+    top: header.offsetHeight,
+    height: `calc(100% - ${header.offsetHeight}px)`
+  });
+  const id = document.querySelector('[data-navigation-json]').dataset.sectionId;
+  const toggleButton = document.querySelector('[data-mobile-menu-toggle]');
 
-  const id = document.querySelector('[data-navigation-json]').dataset.sectionId
   //Component will render
   useEffect(() => {
     document.addEventListener('shopify:section:load', updateModuleData);
@@ -102,17 +116,25 @@ const MainNav = ((props) => {
     document.addEventListener('shopify:section:deselect', closeNavigation);
     document.addEventListener('shopify:block:select', updateSelectedBlock);
     document.addEventListener('navigation:toggle', toggleNavigation);
+    document.addEventListener('breakpointChange', updateHeaderOffset);
     return () => {
       document.removeEventListener('shopify:section:load', updateModuleData);
       document.removeEventListener('shopify:section:select', openNavigation);
       document.removeEventListener('shopify:section:deselect', closeNavigation);
       document.removeEventListener('shopify:block:select', updateSelectedBlock);
       document.removeEventListener('navigation:toggle', toggleNavigation);
+      document.removeEventListener('breakpointChange', updateHeaderOffset);
     }
   }, [])
 
+  useEffect(() => {
+    const action = isOpen ? 'add' : 'remove';
+    toggleButton.classList[action]('is-open');
+    document.querySelector('body').classList[action]('drawer-open');
+  }, [isOpen])
+
   return (
-    <div className={`navigation ${isOpen ? 'is-visible' : ''}`}>
+    <div className={`navigation ${isOpen ? 'is-visible' : ''}`} ref={element} style={styles}>
       <div className="navigation-body" ref={navBody}>
         <NavigationCategories
           key="NavigationCategories"
