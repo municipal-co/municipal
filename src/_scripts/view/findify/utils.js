1d3436f0fb4b40a74fb9b005a1a5285c269bf8a5
betown <joe.gomezr@gmail.com>
2022-12-13 10:37:50 -0300

    Added more utils functions

--- src/_scripts/view/findify/utils.js
+++ src/_scripts/view/findify/utils.js
@@ -50,4 +50,59 @@ const buildUrlSearchParams = (params) => {
   return searchParams;
 }
 
-export { processName, handleize, buildUrlSearchParams }
+const calcOffset = (limit, page) => limit * (page - 1);
+
+const getUrlFilters = () => {
+  const urlParams = new URLSearchParams(document.location.search);
+  const filterList = [];
+
+  for(const [param, value] of urlParams.entries()) {
+    if(param.indexOf('filter') > -1) {
+      const valueList = value.split(',');
+      // Text filters
+      if(param.indexOf('price') == -1  ){
+        filterList.push({
+          name: `${param.indexOf('gender') > -1 ? 'custom_fields.Gender' : param.replace('filter-', '')}`,
+          type: 'text',
+          values: valueList.map(value => {
+              return {value};
+            })
+
+        })
+      } else {
+        // Range filter
+        const valueList = value.split(',');
+        filterList.push({
+          name: param.replace('filter-', ''),
+          type: "range",
+          values: valueList.map(value => {
+            const underscorePosition = value.indexOf('_');
+            const valuePieces = value.split('_');
+            switch(underscorePosition) {
+              case 0:
+                return {to: `${valuePieces[1]}`}
+              case value.length -1 :
+                return {from: `${valuePieces[0]}`}
+              default:
+                return {from: `${valuePieces[0]}`, to: `${valuePieces[1]}`}
+            }
+          })
+        })
+      }
+    }
+  }
+
+  return filterList;
+}
+
+const getURLSort = () => {
+  let sortData = [];
+  const queryParams = new URLSearchParams(document.location.search);
+  if(queryParams.has('sort')){
+    sortData = queryParams.get('sort').split(':');
+  }
+
+  return sortData;
+}
+
+export { processName, handleize, buildUrlSearchParams, calcOffset, getUrlFilters, getURLSort }
